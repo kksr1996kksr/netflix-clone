1b5b0b6c2df047f9d0c3d7bb5c55b661a11612b2
kksr1996 <67324147+kksr1996@users.noreply.github.com>
2020-10-30 20:14:23 +0530

    UI improvements

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
-import Banner from './Banner'
+import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="app">
-      
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
